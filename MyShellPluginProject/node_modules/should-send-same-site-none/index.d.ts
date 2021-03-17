f2f21aa283520d3cc0967b71d49b4342d9f6d490
Holst, Soeren <soeren.holst@sap.com>
2021-03-17 15:34:57 +0100

    Delete unneeded files

--- MyShellPluginProject/node_modules/should-send-same-site-none/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'should-send-same-site-none' {
-  import { RequestHandler } from 'express';
-
-  export const shouldSendSameSiteNone: RequestHandler;
-  export function isSameSiteNoneCompatible(useragent: string): boolean;
-}
